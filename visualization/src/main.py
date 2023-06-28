a46ccdebd5355c5e900f04f853cc582d2a2ad6ef
PrismCC <349604840@qq.com>
2023-06-28 22:17:44 +0800

    python visualization finished

--- visualization/src/main.py
+++ visualization/src/main.py
@@ -1,3 +1,4 @@
+import csv
 import random
 
 import matplotlib.pyplot as plt
@@ -8,51 +9,124 @@ plt.rcParams['font.family'] = 'Consolas'
 
 # 颜色库
 colors = ['#FF817D', '#FFCDD5', '#FFCAA1', '#FFEFA0', '#CBEF96', '#BAFFF8', '#C5E4FF', '#DDC2FF', '#F8D3FF']
-# 球员名
-player_name = ['Aka', 'Bob', 'Chris', 'Daniel', 'Evan', 'Frank', 'George']
+team_color = colors[1:8]
+
+
+# 从csv中读取数据为列表
+def csv_to_list(csv_file):
+    data = []
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            data.append(row)
+    return data
 
 
 # 绘制饼图
-def pie_draw(num, points, team):
-    pie_color = random.sample(colors, num)
-    pie_explode = (0.05, 0.05, 0.05, 0.05, 0.05)
-    plt.pie(x=points, labels=random.sample(player_name, num), colors=pie_color, radius=1, shadow=False,
+def pie_draw(data, label, title):
+    pie_explode = (0.05,) * len(data)
+    plt.pie(x=data, labels=label, colors=team_color, radius=1, shadow=False,
             explode=pie_explode, autopct='%3.1f%%')
-    title = "Data of " + team
     plt.title(title)
-    plt.savefig('./picture/' + title, dpi=600, bbox_inches='tight')
+    plt.savefig('output/' + title, dpi=600, bbox_inches='tight')
+    plt.show()
+
+
+def barh_draw(data, label, color, title):
+    ax = plt.axes()
+    ax.barh(label, data, color=color)
+    ax.set_title(title)
+    ax.set_xlabel("Ability points")
+    ax.set_ylabel("Player")
+    ax.grid(axis='x')
+    ax.set_xlim(80, 140)
+    plt.savefig('output/' + title, dpi=600, bbox_inches='tight')
     plt.show()
 
 
 # 绘制雷达图
-def radar_draw(num, label, data, player):
-    angle = np.linspace(0, np.pi * 2, num, endpoint=False)
+def radar_draw( data, label, player):
+    angle = np.linspace(0, np.pi * 2, 5, endpoint=False)
     radar_data = np.concatenate((data, [data[0]]))
     radar_label = np.concatenate((label, [label[0]]))
     angle = np.concatenate((angle, [angle[0]]))
     single_color = random.choice(colors)
     ax = plt.subplot(111, polar=True)
-    ax.plot(angle, radar_data, 'o-', linewidth=0.8, color=single_color, alpha=0.4)
+    ax.plot(angle, radar_data, 'o-', linewidth=0.8, color=single_color, alpha=0.8)
     ax.fill(angle, radar_data, alpha=0.15, color=single_color)
     ax.set_thetagrids(angle * 180 / np.pi, labels=radar_label)
     ax.set_theta_zero_location('N')
-    ax.set_rlim(0, 25)
+    ax.set_rlim(0, 100)
     title = 'Data of ' + player
     ax.set_title(title)
     plt.legend([player], loc='best')
-    plt.savefig('./picture/' + title, dpi=600, bbox_inches='tight')
+    plt.savefig('output/' + title, dpi=600, bbox_inches='tight')
     plt.show()
 
 
-def main():
-    pie_num = 5
-    pie_player_points = np.random.randint(5, 30, pie_num)
-    pie_draw(pie_num, pie_player_points, 'Team A')
+def team():
+    team_data = csv_to_list(r"output\teams.csv")
+    team_name = []
+    team_winner = []
+    team_points = []
+    for i in team_data:
+        team_name.append(i[0])
+        team_winner.append(i[1])
+        team_points.append(i[2])
 
-    radar_num = 5
-    radar_data = np.random.randint(5, 20, radar_num)
-    radar_label = np.array(['Scores', 'Rebounds', 'Assists', 'Steals', 'Stability'])
-    radar_draw(radar_num, radar_label, radar_data, 'Player X')
+    team_winner = list(map(float, team_winner))
+    team_points = list(map(float, team_points))
+    # 第四队和第六队补正
+    team_winner[3] *= 1.2
+    team_winner[5] *= 1.2
+    team_points[3] *= 1.2
+    team_points[5] *= 1.2
+
+    pie_draw(team_winner, team_name, "winner_pie")
+    pie_draw(team_points, team_name, "points_pie")
+
+
+def top_players():
+    players_data = csv_to_list(r"output\top-nine player.csv")
+    player_name = []
+    player_points = []
+    player_team = []
+    team_name = ["Hawks", "Magic", "Nets", "Wizards", "Trail Blaze", "Grizzlies", "Knicks"]
+    for i in players_data:
+        player_name.append(i[0])
+        player_points.append(int(i[1]))
+        player_team.append(i[2])
+    player_color = [colors[team_name.index(i) + 1] for i in player_team]
+    barh_draw(player_points[::-1], player_name[::-1], player_color[::-1], "top-nine player")
+
+
+def single_player():
+    single_players = ["Brook Lopez", "Nikola Vucevic", "Al Horford"]
+    for player in single_players:
+        player_data = csv_to_list("output/" + player + ".csv")
+        records = scores = assists = rebounds = steals = 0
+        points = []
+        for record in player_data:
+            records += 1
+            scores += int(record[0])
+            assists += int(record[1])
+            rebounds += int(record[2])
+            steals += int(record[3])
+            points.append(scores * 3 + assists + rebounds + steals)
+        data = [(scores / records ) * 4, (assists / records + 1) * 16, (rebounds / records) * 7.5,
+                (steals / records + 1) * 40, 5000 / np.var(np.array(points)) * 100]
+        print(player, data)
+        label=['Scores', 'Rebounds', 'Assists', 'Steals', 'Stability']
+        radar_draw(data,label, player)
+
+
+
+def main():
+    # radar_num = 5
+    # radar_data = np.random.randint(5, 20, radar_num)
+    # radar_label = np.array(['Scores', 'Rebounds', 'Assists', 'Steals', 'Stability'])
+    # radar_draw(radar_num, radar_label, radar_data, 'Player X')
+    single_player()
 
 
 if __name__ == '__main__':
--- visualization/src/table_process.py
+++ visualization/src/table_process.py
@@ -1,7 +1,124 @@
 # 从xls文件中读取数据
 import openpyxl
+from PIL import Image
 
 xlsx_path = r"table\python_read.xlsx"
 
-workbook = openpyxl.load_workbook(xlsx_path)
-print(workbook.sheetnames)
+
+def test():
+    workbook = openpyxl.load_workbook(xlsx_path)
+    print(workbook.sheetnames)
+
+
+def team_process():
+    team_num = 7
+    workbook = openpyxl.load_workbook(xlsx_path)
+    team_sheet = workbook['Team']
+    team_name = "Hawks,Magic,Nets,Wizards,Trail Blaze,Grizzlies,Knicks".split(',')
+
+    points_array = []
+    for i in range(team_num):
+        line = []
+        for j in range(team_num):
+            v = team_sheet.cell(row=i + 2, column=j + 2).value
+            line.append(0 if v is None else v)
+        points_array.append(line)
+    print(points_array)
+
+    class Data:
+        def __init__(self, name, win, points):
+            self.name = name
+            self.win = win
+            self.points = points
+
+    team_list = []
+    for i in range(team_num):
+        team = Data(team_name[i], 0, 0)
+        for j in range(team_num):
+            team.points += points_array[i][j]
+            team.win += 1 if points_array[i][j] > points_array[j][i] else 0
+        team_list.append(team)
+
+    with open(r"output\teams.csv", "w") as out:
+        for i in team_list:
+            out.write(f"{i.name},{i.win},{i.points},\n")
+
+    img1 = Image.new('RGBA', (team_num * 100, team_num * 100))
+    img2 = Image.new('RGBA', (team_num * 100, team_num * 100))
+    for i in range(team_num * 100):
+        for j in range(team_num * 100):
+            y, x = i // 100, j // 100
+            red = (points_array[y][x] + points_array[x][y] - 180) * 3 if points_array[y][x] != 0 else 0
+            blue = (points_array[y][x] - points_array[x][y]) * 6 if points_array[y][x] != 0 else 0
+            green = (points_array[x][y] - points_array[y][x]) * 6 if points_array[y][x] != 0 else 0
+            alpha = 64
+            img1.putpixel((i, j), (red, 0, 0, alpha))
+            img2.putpixel((i, j), (0, green, blue, alpha))
+    img1.save(r'output\team_pic1.png')
+    img2.save(r'output\team_pic2.png')
+
+
+def player_process():
+    workbook = openpyxl.load_workbook(xlsx_path)
+    player_sheet = workbook['Player']
+    line_no = 2
+    player_dic = {}
+
+    class Data:
+        def __init__(self):
+            self.records = 0
+            self.scores = 0
+            self.assists = 0
+            self.rebounds = 0
+            self.steals = 0
+            self.points = 0
+            self.name = ""
+
+        def cal_points(self):
+            self.points = (self.scores * 3 + self.assists + self.rebounds + self.steals) // self.records \
+                          + self.records * 10
+
+    empty_line = False
+    while True:
+        name = player_sheet.cell(row=line_no, column=1).value
+        if name is not None:
+            empty_line = False
+        elif empty_line is False:
+            empty_line = True
+            line_no += 1
+            continue
+        else:
+            break
+
+        if not name in player_dic:
+            player_dic[name] = Data()
+        player_dic[name].records += 1
+        score = player_sheet.cell(row=line_no, column=2).value
+        assist = player_sheet.cell(row=line_no, column=3).value
+        rebound = player_sheet.cell(row=line_no, column=4).value
+        steal = player_sheet.cell(row=line_no, column=5).value
+        player_dic[name].scores += score if type(score) == type(1) else 0
+        player_dic[name].assists += assist if type(assist) == type(1) else 0
+        player_dic[name].rebounds += rebound if type(rebound) == type(1) else 0
+        player_dic[name].steals += steal if type(steal) == type(1) else 0
+        line_no += 1
+
+    player_list = []
+    for player, data in player_dic.items():
+        data.cal_points()
+        data.name = player.strip()
+        player_list.append(data)
+    player_list.sort(key=lambda x: x.points, reverse=True)
+
+    with open(r"output\players.csv", "w") as out:
+        for i in player_list:
+            print(i.name + ": " + i.points.__str__())
+            out.write(f"{i.name},{i.points},{i.records},{i.scores},{i.assists},{i.rebounds},{i.steals},\n")
+
+
+def main():
+    team_process()
+
+
+if __name__ == '__main__':
+    main()
